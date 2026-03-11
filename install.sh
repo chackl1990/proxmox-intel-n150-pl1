16fc46396881c97103282b67ab4cffb071f02013
chackl <mailmaster@chackl.com>
2026-03-11 11:42:52 +0100

    Update install script to download files from repository

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 set -e
 
+REPO="https://raw.githubusercontent.com/chackl1990/proxmox-intel-n150-pl1/main"
+
 echo "Installing rapl-pl1-linear..."
 
-install -m 755 rapl-pl1-linear.sh /usr/local/bin/rapl-pl1-linear.sh
-install -m 644 rapl-pl1-linear.service /etc/systemd/system/rapl-pl1-linear.service
+echo "Downloading controller script..."
+wget -qO /usr/local/bin/rapl-pl1-linear.sh "$REPO/rapl-pl1-linear.sh"
 
+echo "Downloading systemd service..."
+wget -qO /etc/systemd/system/rapl-pl1-linear.service "$REPO/rapl-pl1-linear.service"
+
+chmod +x /usr/local/bin/rapl-pl1-linear.sh
+
+echo "Reloading systemd..."
 systemctl daemon-reload
-systemctl enable --now rapl-pl1-linear
+
+echo "Enabling service..."
+systemctl enable --now rapl-pl1-linear.service
 
 echo "Installation complete."
